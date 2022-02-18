475b6b9b5acc1e54d94820449f965388b975a137
escoobi <gustavo_o_@hotmail.com>
2022-02-18 10:34:42 -0400

    Caso de uso de Interface em p.O.O

---
+++ src/br/com/dio/Calculadora.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+public class Calculadora implements OperacaoMatematica{
+
+
+    @Override
+    public double soma(double operando1, double operando2) {
+        return operando1 + operando2;
+    }
+
+    @Override
+    public double subtracao(double operando1, double operando2) {
+        return operando1 - operando2;
+    }
+
+    @Override
+    public double divisao(double operando1, double operando2) {
+        return operando1 / operando2;
+    }
+
+    @Override
+    public double multiplicacao(double operando1, double operando2) {
+        return operando1 * operando2;
+    }
+}
---
+++ src/br/com/dio/OperacaoMatematica.java
@@ -0,0 +1,8 @@
+package br.com.dio;
+
+public interface OperacaoMatematica {
+    double soma(double operando1, double operando2);
+    double subtracao(double operando1, double operando2);
+    double divisao(double operando1, double operando2);
+    double multiplicacao(double operando1, double operando2);
+}
